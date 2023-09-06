e6072e5c20aa4e416091f975ba0c22e83f10e0a2
Joshua Chamberlain <jospcham@ut.utm.edu>
2023-09-06 14:25:06 -0500

    Added functionality to sensors

--- Project/app.R
+++ Project/app.R
@@ -17,14 +17,11 @@ ui <- fluidPage(
   ),
   fluidRow(
     column(width = 5,  # Adjust the width as needed
-           leafletOutput("map", width = "100%", height = "50vh"),
-           sliderInput("Date", "Dates", step = 7, 
-                       min = as.Date("01-01-2023", "%m-%d-%Y"), 
-                       max = as.Date("12-12-2023", "%m-%d-%Y"), 
-                       value = as.Date("01-01-2023", "%m-%d-%Y"))),
+           leafletOutput("map", width = "100%", height = "50vh")
+           ),
     column(width = 4, 
-           plotOutput("sensorPlot")
-    ),
+           plotOutput("clickedSensorPlot")
+    )
   )
 )
 
@@ -34,8 +31,14 @@ server <- function(input, output, session) {
     return(csv_data)
   }
   
+  markers_data <- data.frame(
+    lat = c(35.407478, 35.408833, 35.409777, 35.410766, 35.409819, 35.411284, 35.409485, 35.408433),
+    lng = c(-89.390113, -89.391825, -89.391935, -89.391956, -89.392733, -89.389521, -89.386922, -89.388013),
+    label = c("Marker 1", "Marker 2", "Marker 3", "Marker 4", "Marker 5", "Marker 6", "Marker 7", "Marker 8")
+  )
+  
   csv_data <- reactivePoll(
-    intervalMillis = 100,  # Update interval in milliseconds 
+    intervalMillis = 1000,  # Update interval in milliseconds 
     session = session,
     checkFunc = function() {
       Sys.time()  # Always return current time to trigger updates
@@ -56,25 +59,45 @@ server <- function(input, output, session) {
       setView(lng = -89.389436, lat = 35.409276, zoom = 16) %>%
       setMaxBounds(lng1 = -89.389436, lat1 = 35.409276, 
                    lng2 = -89.389436, lat2 = 35.40926) %>%
-      addCircleMarkers(lng = -89.390113, lat = 35.407478, popup = "Marker 1") %>%
-      addCircleMarkers(lng = -89.391825, lat = 35.408833, popup = "Marker 2") %>%
-      addCircleMarkers(lng = -89.391935, lat = 35.409777, popup = "Marker 3") %>%
-      addCircleMarkers(lng = -89.391956, lat = 35.410766, popup = "Marker 4") %>%
-      addCircleMarkers(lng = -89.392733, lat = 35.409819, popup = "Marker 5") %>%
-      addCircleMarkers(lng = -89.389521, lat = 35.411284, popup = "Marker 6") %>%
-      addCircleMarkers(lng = -89.386922, lat = 35.409485, popup = "Marker 7") %>%
-      addCircleMarkers(lng = -89.388013, lat = 35.408433, popup = "Marker 8")
+      addCircleMarkers(
+        data = markers_data,
+        lat = ~lat,
+        lng = ~lng,
+        label = ~label,
+        radius = 10
+      )
   })
   
-  output$sensorPlot <- renderPlot({
+  clicked_marker <- reactiveVal(NULL)
+  
+  observeEvent(input$map_marker_click, {
+    click_info <- input$map_marker_click
+    marker_index <- which(
+      markers_data$lat == click_info$lat & markers_data$lng == click_info$lng
+    )
+    if (length(marker_index) == 1) {
+      clicked_marker(markers_data$label[marker_index])
+    } else {
+      clicked_marker(NULL)
+    }
+  }, ignoreInit = TRUE)
+  
+  output$clickedSensorPlot <- renderPlot({
     data <- csv_data()
-    sensorData <- data$`Sensor.1`
-    plot(sensorData, type = "l", 
-         xlab = "Time",
-         ylab = "Sensor 1 Irradiance",
-         main = "Sensor 1 Plot",
-         xlim = c(1, 1440), ylim = c(-10, max(550, max(sensorData + 10))))
+    if(!is.null(clicked_marker())) {
+      marker_label <- clicked_marker()
+      column_name <- paste0("Sensor.", gsub("Marker ", "", marker_label))
+      if (column_name %in% colnames(data)) {
+        sensorData <- data[, column_name]
+        plot(sensorData, type = "l",
+             xlab = "Time",
+             ylab = paste("Sensor", gsub("Marker ", "", marker_label), "Irradiance"),
+             main = paste(marker_label, "Plot"),
+             xlim = c(1, 1440), ylim = c(-10, max(550, max(sensorData + 10))))
+      }
+    }
   })
+  
 }
 
 shinyApp(ui, server)
